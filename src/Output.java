0a0118202a11172e09f637c37f0c2b210ba66fab
steffen.jaser <steffen.jaser@aracom.de>
2022-12-19 11:22:44 +0100

    fix output

--- src/Output.java
+++ src/Output.java
@@ -1,5 +1,5 @@
 public interface Output {
-        public static void sayHello (String outputString) {
-        System.out.println("Hello %s!" + outputString);
+    public static void sayHello(String outputString) {
+        System.out.printf("Hello %s!", outputString);
     }
 }
