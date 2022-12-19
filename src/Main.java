ace530066814b21c3239b0f5fa31df4ee84a9a7b
Maxwell <maximilian.claus@aracom.de>
2022-12-19 10:43:23 +0100

    Hilfe Methode

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        help();
         System.out.println("Hello World!");
+    }
+
+    public static String help(){
+       return "Dieses Programm nimmt einen Parameter entgegen und gibt diesen aus";
     }
 }
