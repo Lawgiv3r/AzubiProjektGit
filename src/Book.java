2fc6e3811d15cbe0405bbbb304cd9feb2d40727d
steffen.jaser <steffen.jaser@aracom.de>
2022-12-21 10:11:22 +0100

    create book class base

---
+++ src/Book.java
@@ -0,0 +1,18 @@
+public class Book {
+    private String name;
+    private String mainCharacter;
+
+    public Book(String name, String mainCharacter) {
+        this.name = name;
+        this.mainCharacter = mainCharacter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMainCharacter() {
+        return mainCharacter;
+    }
+
+}
