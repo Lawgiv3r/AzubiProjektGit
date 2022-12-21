8e9a26ccd2d8893bc91e7e335ed27aa1a2c85b6a
Sven Hassmann <svenhassmann.sh@gmail.com>
2022-12-21 11:22:58 +0100

    commit1

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         // call createTxtFile
         try {
+            //haha habe ich leerzeichen gemacht 
             createTxtFile("Test");
         } catch (Exception e) {
             System.out.println("Exception");
